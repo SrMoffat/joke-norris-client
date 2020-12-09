983f0635ed38013075427f21dbf61fe17bedb646
SrMoffat <ngigemoffat@gmail.com>
2020-12-09 10:45:02 +0300

    add category icons

--- src/components/CategoryCard.js
+++ src/components/CategoryCard.js
@@ -9,19 +9,40 @@ const StyledCategoryCard = styled.div`
     cursor: pointer;
     user-select: none;
     transition: all .1s;
+    color: #6e6e6e;
     &:hover {
         transform: translateY(-.025rem);
         box-shadow: 0rem .3rem .4rem 0rem rgba(0,0,0,0.1);
+        
     }
     &:active {
         transform: translateY(.025rem);
         box-shadow: 0rem .2rem .2rem 0rem rgba(0,0,0,0.2);
     }
 `;
+const StyledCategoryCardContent = styled.div`
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    align-items: center;
+`;
+const StyledCategoryName = styled.span`
+    justify-self: start;
+    font-weight: 500;
+`;
+const categoryAvatarStyles = {
+    height: 40,
+    width: 40, 
+    justifySelf: "end"
+};
 const CategoryCard = ({ category }) => {
+    console.log("CAT", category);
+    console.log("TYPE", typeof category);
     return (
         <StyledCategoryCard>
-            { capitalizeFirstLetter(category) }
+            <StyledCategoryCardContent>
+                <StyledCategoryName>{ capitalizeFirstLetter(category) }</StyledCategoryName>
+                <img style={categoryAvatarStyles} src={`/icons/${category}.svg`} alt={`${category}`} />
+            </StyledCategoryCardContent>
         </StyledCategoryCard>
     );
 };
